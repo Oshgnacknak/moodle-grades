cf2c16dc2bc3b91e2e55042c383022f6352ee372
Oshgnacknak <osh@oshgnacknak.de>
2021-07-26 00:17:18 +0200

    Public files

---
+++ public/main.js
@@ -0,0 +1,59 @@
+
+function drawLineCharts(course, grades) {
+    const header = document.createElement('h2');
+    header.innerText = course.shortname;
+    document.body.appendChild(header);
+
+    const canvas = document.createElement('canvas');
+    document.body.appendChild(canvas);
+
+    const chart = new Chart(canvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: grades.map((_, i) => i),
+            datasets: [{ 
+                data: grades.map(g => g && g.grade),
+                label: 'Grade',
+                borderColor: '#3cba9f',
+                backgroundColor: '#71d1bd',
+                fill: false,
+              }, { 
+                data: grades.map(g => g && g.max),
+                label: 'Max',
+                borderColor: '#3e95cd',
+                backgroundColor: '#7bb6dd',
+                fill: false,
+              }]
+        }});
+}
+
+const parseGrades = (grades) =>
+    grades.tabledata.map(g => {
+        if (g.grade !== undefined && isNaN(g.grade.content)) {
+            const grade = parseFloat(g.grade.content);
+            const max = parseFloat(g.range.content.split(/;/g)[1]);
+            return { grade, max };
+        }
+        return null;
+    });
+
+document.addEventListener('DOMContentLoaded', () => {
+    axios.get('/api/info')
+    .then(res => res.data)
+    .then(info =>
+        axios.get(`/api/user/${info.userid}/courses/`)
+            .then(res => res.data)
+            .then(courses => {
+                fullname.innerText = info.fullname;
+
+                for (const course of courses) {
+                    axios.get(`/api/user/${info.userid}/grades/${course.id}`)
+                    .then(res => res.data)
+                    .then(grades => 
+                        drawLineCharts(course, parseGrades(grades)));
+                }
+
+                loading.remove();
+            })
+    );
+});
